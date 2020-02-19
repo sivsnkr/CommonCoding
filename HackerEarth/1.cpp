5cf9d2c7cb04723013a1ad943465a050b18ea08c
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-02-19 23:32:21 +0530

    19-02-2020

--- HackerEarth/1.cpp
+++ HackerEarth/1.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void kthPlaindrome(long int n, long int k, long  int sum, long int i,int s[]){
+void kthPlaindrome(long int n, long int k, long int sum, long int i,int s[]){
     s[0] = 1;
-    int j;
+    long int j;
     for(j = 1; j <= i-2; j++){
         s[j] = 0;
     }
@@ -12,21 +12,15 @@ void kthPlaindrome(long int n, long int k, long  int sum, long int i,int s[]){
 
     k-=sum;
 
-    // cout<<"sum "<<sum<<endl;
-
-    // cout<<"k "<<k<<endl;
-
-    for(j = 0; j <= ceil(float(i)/2)-1; j++){
-        if(j == 0){
-            s[0] = ceil(float(k)/pow(10,ceil(float(i)/2)-1));            
-            k-=(s[0]-1)*pow(10,ceil(float(i)/2)-1);
-        }else{
-            s[j] = k/pow(10,ceil(float(i)/2)-1-j)-1;
-            k-=((s[j]+1)*pow(10,ceil(float(i)/2)-1-j));
+    for(j = 0; j <= (i-1)/2; j++){
+        if(j == 0){            s[0] = ceil(double(k)/pow(10,(i-1)/2));
+            k-=(s[0]-1)*pow(10,(i-1)/2);
+        }else{            s[j] = ceil(double(k)/pow(10,(i-1)/2-j))-1;
+            k-=(s[j]*pow(10,(i-1)/2-j));
         }
     }
 
-    for(j = 0; j <= ceil(float(i)/2)-1; j++){
+    for(j = 0; j < i/2; j++){
         s[i-j-1] = s[j];
     }
 }
@@ -38,16 +32,17 @@ long long int decodePlainDrome(long long int n){
     long long int sum = 1;
     int i;
     for(i = 1; i < length; i++){
-        sum+=9*pow(10,ceil(float((i-1)/2)));
+        sum+=9*pow(10,(i-1)/2);
     }
 
-    int power = ceil(float(length)/2)-1;
+    int power = ceil(double(length)/2)-1;
 
-    for(i = 0; i <= ceil(float(length)/2)-1; i++){
-        int num = s[i]-48;        if(i == 0){
+    for(i = 0; i <= ceil(double(length)/2)-1; i++){
+        int num = s[i]-48;
+        if(i == 0){
             sum+=(num-1)*pow(10,power);
         }else{
-            sum+=(num+1)*pow(10,power);
+            sum+=num*pow(10,power);
         }
         power--;
     }
@@ -73,28 +68,23 @@ long long int giveNearstPlain(long long int x){
     string t = "";
     int size = s.length();
 
-    if(size == 2|| size == 1){
-        while(!isPlain(x)){
-            x--;
-        }
-        return x;
-    }
-
     int i;
-    t+=s[0];
-    for(i = 1; i < s.length()-1; i++){
-        t+=to_string(min(s[i]-48,s[size-i-1]-48));
+
+    for(i = 0; i <= (size-1)/2; i++){
+        t+=s[i];
     }
 
-    t+=t[0];
+    for(i = size/2-1; i >= 0; i--){
+        t+=s[i];
+    }
 
     long long int num = stoll(t);
 
     if(num>x){
-        num -= pow(10,size/2);
+        return decodePlainDrome(x)-1;
+    }else{
+        return decodePlainDrome(x);
     }
-
-    return num;
 }
 
 int main(){
@@ -108,23 +98,18 @@ int main(){
         if(isPlain(x))
             k--;
 
-        x = giveNearstPlain(x);
-        // cout<<"x "<<x<<endl;
-        long long int index = decodePlainDrome(x);
-
-        // cout<<"INdex "<<index<<endl;
+        long long int index = giveNearstPlain(x);
         k+=index;
         long int sum = 0;
         long int i = 1;
         while(1){
-            long int res = 9*pow(10,ceil(float((i-1)/2)));
+            long int res = 9*pow(10,(i-1)/2);
             if(sum+res < k)
                 sum+=res;
             else
                 break;
             i++;
         }
-
 
         int s[i];
 
@@ -136,7 +121,4 @@ int main(){
             cout<<s[j];
         }
         cout<<endl;
-    }
-
-    // cout<<s<<endl;
-}
+    }}

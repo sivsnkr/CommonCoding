1fc667c6dd6865ec440bcc37a0e0ae647db79e61
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-02-19 10:49:06 +0530

    18-02-2020

--- HackerEarth/1.cpp
+++ HackerEarth/1.cpp
@@ -1,21 +1,101 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void kthPlaindrome(long int n, long int k, long  int sum, long int i,int s[]){
+    while(1){
+        long int res = 9*pow(10,ceil(float((i-1)/2)));
+        if(sum+res < k)
+            sum+=res;
+        else
+            break;
+        i++;
+    }
+
+    s[0] = 1;
+    int j;
+    for(j = 1; j <= i-2; j++){
+        s[j] = 0;
+    }
+
+    s[i-1] = 1;
+
+
+    // cout<<"i "<<i<<endl;
+
+    // for(j = 0; j < i; j++){
+    //     cout<<s[j];
+    // }
+
+    // cout<<endl;
+
+    k-=sum;
+
+    // cout<<"Sum "<<sum<<endl;
+
+    // cout<<"K "<<k<<endl;
+
+    for(j = 0; j <= ceil(float(i)/2)-1; j++){
+        if(j == 0){
+            s[0] = ceil(float(k/pow(10,ceil(float(i)/2)-1)));            
+            k-=(s[0]-1)*pow(10,ceil(float(i)/2)-1);
+        }else{
+            s[j] = k/pow(10,ceil(float(i)/2)-1-j)-1;
+            k-=((s[j]+1)*pow(10,ceil(float(i)/2)-1-j));
+        }
+    }
+
+    for(j = 0; j <= ceil(float(i)/2)-1; j++){
+        s[i-j-1] = s[j];
+    }
+}
+
+bool isPlain(long int n){
+    string s = to_string(n);
+
+    int size = s.length();
+    for(int i = 0; i < size/2; i++){
+        if(s[i]!=s[size-i-1]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main(){
-    int n;
-    cin>>n;
+    int t;
+    cin>>t;
 
-    vector<long double> x(n);
+    while(t--){
+        long int x,k;
+        cin>>x>>k;
+        while(!isPlain(x)){
+            x--;
+            k++;
+        }
 
-    int i;
-    for(i = 0; i < n; i++){
-        cin>> x[i];
+        k+=(x-1);
+
+        long int sum = 0;
+        long int i = 1;
+        // long int n = x;
+        while(1){
+            long int res = 9*pow(10,ceil(float((i-1)/2)));
+            if(sum+res < k)
+                sum+=res;
+            else
+                break;
+            i++;
+        }
+        int s[i];
+
+        kthPlaindrome(x,k,sum,i,s);
+
+        int j;
+
+        for(j = 0; j < i; j++){
+            cout<<s[j];
+        }
+        cout<<endl;
     }
-
-    long double sum = 0;
-
-    for(i = 0; i < n; i++){
-        sum += (float(n-1)/2)*sin(2*x[i]);
-    }
-    printf("%.2Lf\n", sum);
 }
--- HackerEarth/2.cpp
+++ HackerEarth/2.cpp
@@ -1,16 +1,147 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long int getInt(int arr[],int size){
+    long long int sum = 0;
+    for(int i = 0; i < size/2; i++){
+        arr[size-i-1] = arr[i];
+    }
+
+    // string s = "";
+
+    int power = 0;
+
+    bool start = false;
+
+    for(int i = size-1; i >= 0; i--){
+        // sum+=arr[i]*pow(10,10-i-1);
+        if(arr[i]!=0||start){
+            sum+=arr[i]*pow(10,power);
+            power++;
+            start = true;
+        }
+    }
+
+    // for(int i = 0; i <= 9; i++){
+    //     cout<<arr[i];
+    // }
+
+    // cout<<endl;
+
+    // cout<<"return sum"<<endl;
+
+    // cout<<sum<<endl;
+
+    // long long int num = stoll(s);
+    // cout<<s<<endl;
+
+    // cout<<num<<endl;
+
+    return sum;
+}
+
+long int binarySearch(vector<long long int> a, long long int value){
+    long start = 0,end = a.size()-1;
+
+    while(start<end){
+        long int mid = (start+end)/2;
+
+        if(a[mid] == value)
+            return mid;
+        else if(a[mid] > value)
+            end = mid-1;
+        else
+            start = mid+1;
+    }
+
+    return start;
+}
+
 int main()
 {
-    for (int i = 1; i <= 10; i++)
-    {
-        for (int j = 1; j <= 10; j++)
-        {
-            if (i + j == 12)
-            {
-                cout << i << " " << j << endl;
+    // int t;
+    // cin>>t;
+
+    int i,j,k,l,m;
+
+    int arr[18]={0};
+    int arr1[17]={0};
+
+    vector<long long int> a;
+
+    for(i = 0; i <=9; i++){
+        arr[0] = i;
+        for(j = 0; j <=9; j++){
+            arr[1] = j;
+            for(k = 0; k <=9; k++){
+                arr[2] = k;
+                for(l = 0; l <= 9; l++){
+                    arr[3] = l;
+                    for(m = 1; m <=9; m++){
+                        for(int s = 0; s <= 9; s++){
+                            for(int o = 0; o <=9; o++){
+                                for(int p = 0; p <= 9; p++){
+                                    for(int q = 0; q <= 9; q++){
+                                        // for(int r= 0; r <=9; r++){
+                                        //     //fsf
+                                        // }
+                                    }
+                                }
+                            }
+                        }
+                        // arr[4] = m;
+                        // long long int num = getInt(arr,10);
+                        // a.push_back(num);
+                    }
+                }
             }
         }
     }
+
+    // for(i = 0; i <=9; i++){
+    //     arr1[0] = i;
+    //     for(j = 0; j <=9; j++){
+    //         arr1[1] = j;
+    //         for(k = 0; k <=9; k++){
+    //             arr1[2] = k;
+    //             for(l = 0; l <= 9; l++){
+    //                 arr1[3] = l;
+    //                 for(m = 0; m <=9; m++){
+    //                     arr1[4] = m;
+    //                     for(int s = 0; s <=9; s++){
+    //                         arr1[5] = s;
+
+    //                         long long int num = getInt(arr1,11);
+    //                         a.push_back(num);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    // for(i = 1; i <= 9; i++){
+    //     a.push_back(i);
+    // }
+
+    // sort(a.begin(),a.end());
+
+    // for(i = 0; i < t; i++){
+    //     long int x,k;
+    //     cin>>x>>k;
+
+    //     long int index = binarySearch(a,x);
+    //     bool res = a[index]==x;
+    //     // cout<<"Index "<<index<<" "<<res<<endl;
+    //     if(a[index]<x)
+    //         index++;
+    //     cout<<a[index+k-1]<<endl;
+    // }
+
+    // cout<<a[0]<<" "<<a[1]<<endl;
+
+    // for(i = 1; i <= 32; i++){
+    //     cout<<a[i]<<" ";
+    // }
+    // cout<<endl;
 }

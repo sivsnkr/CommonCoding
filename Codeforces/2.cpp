24895e78ed0180bb1de51c9113d8368c1640ac3c
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-02-24 15:18:09 +0530

    24-02-2020

--- Codeforces/2.cpp
+++ Codeforces/2.cpp
@@ -5,7 +5,17 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int n,x,y;
-        int bestp,worstp;
+        long int n,x,y;
+        cin>>n>>x>>y;
+        long int bestp,worstp;
+        bestp = min(x,y);
+        if(bestp>1)
+        	bestp--;
+
+        worstp = x+y-1;
+        if(worstp>n)
+        	worstp = n;
+
+        cout<<bestp<<" "<<worstp<<endl;
     }
 }

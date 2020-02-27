1aa940d0c7242d9aa736236ed8df6cd6c12b4bab
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-02-27 13:36:42 +0530

    26-02-2020

--- Geeksforgeeks/1.cpp
+++ Geeksforgeeks/1.cpp
@@ -1,64 +1,42 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-int dsrc,ddest,totalSum;
-int minSum = INT_MAX;
+int res(int a,int b){
+	queue<pair<int,int>> q;
+	q.push({a,0});
 
-int dfs(int src,vector<unordered_set<int>> &nodes, vector<bool> &visit, vector<int> &weight, bool getNodes){
-	if(nodes[src].size()==0)
-		return weight[src];
+	unordered_set<int> visit;
+	visit.insert(a);
+	while(!q.empty()){
+		pair<int,int> curnode = q.front();
+		q.pop();
 
-	int sum = weight[src];
-	visit[src] = true;
+		int x = curnode.first;
+		int level = curnode.second;
 
-	unordered_set<int>::iterator it;
+		if(2*x == b|| x-1 == b)
+			return level+1;
 
-	for(it = nodes[src].begin(); it != nodes[src].end(); it++){
-		int sum1 = 0;
-		if(!visit[*it]){
-			sum1=dfs(*it,nodes,visit,weight,getNodes);
-			sum+=sum1;
-			if(getNodes){
-				int bsum = totalSum-sum1;
-				if(abs(bsum-sum1)<minSum){
-					minSum = abs(bsum-sum1);
-					dsrc = src;ddest = *it;
-				}
-			}
+		if(visit.find(2*x) == visit.end()){
+			visit.insert(2*x);
+			q.push({2*x,level+1});
+		}
+		if(visit.find(x-1) == visit.end()){
+			visit.insert(x-1);
+			q.push({x-1,level+1});
 		}
 	}
-
-	return sum;
+	return 0;
 }
+int main(){
+	int a,b;
+	cin>>a>>b;
 
-int main()
-{
-	int n;
-	cin>>n;
-
-	int i;
-
-	vector<unordered_set<int>> nodes(n);
-	vector<int> weight(n);
-	for(i = 0; i < n-1; i++){
-		int src,dest;
-		cin>>src>>dest;
-
-		nodes[src].insert(dest);
+	int r = res(a,b);
+	if(r != 0){
+		cout<<r<<endl;
+	}else{
+		cout<<"Not possible "<<endl;
 	}
-
-	cout<<"Enter the weights of nodes"<<endl;
-
-	for(i = 0; i < n; i++){
-		cin>>weight[i];
-	}
-
-	vector<bool> visit(n,false);
-
-	totalSum = dfs(0,nodes,visit,weight,false);
-
-	visit.assign(n,false);
-	
-	dfs(0,nodes,visit,weight,true);
-
-	cout<<dsrc<<" "<<ddest<<endl;}
+}

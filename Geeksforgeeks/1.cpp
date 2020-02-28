572987604c7a951c338ac0f2a42bc1ec0c2a8325
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-02-28 09:29:22 +0530

    28-02-2020

--- Geeksforgeeks/1.cpp
+++ Geeksforgeeks/1.cpp
@@ -1,29 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int bfs(vector<set<int>> &nodes, int size){
-	queue<pair<int,int>> q;
-	q.push({0,0});
+int bfs(int number){
+	queue<string> q;
+	string t = "1";
+	q.push(t);
 
-	vector<bool> visit(size,false);
-	visit[0] = true;
 	while(!q.empty()){
-		int curnode = q.front().first;
+		string curn = q.front();
 
-		int level = q.front().second;
+		int currNumber = stoi(curn);
 
 		q.pop();
 
-		set<int>::iterator it;
+		if(currNumber%number == 0)
+			return currNumber;
 
-		for(it = nodes[curnode].begin(); it != nodes[curnode].end(); it++){
-			if(*it == size - 1)
-				return level+1;
-			else{
-				visit[*it] = true;
-				q.push({*it,level+1});
-			}
-		}
+		q.push(curn+"1");
+		q.push(curn+"0");
 	}
 
 	return -1;
@@ -34,64 +28,7 @@ int main()
 	int n;
 	cin >> n;
 
-	vector<int> elements(n);
 
-	int i;
-	for(i = 0; i < n; i++){
-		cin>>elements[i];
-	}
-
-	unordered_map<int,set<int>> connectingNodes(n);
-
-	unordered_map<int,set<int>>::iterator it;
-	set<int>::iterator it1;
-
-	set<int> temp;
-
-	for(i = 0; i < n; i++){
-		if(connectingNodes.find(elements[i]) == connectingNodes.end()){
-			temp.insert(i);
-			connectingNodes.insert({elements[i],temp});
-			temp.clear();
-		}else{
-			connectingNodes[elements[i]].insert(i);
-		}
-	}
-
-	vector<set<int>> nodes(n);
-
-	for(it = connectingNodes.begin(); it != connectingNodes.end(); it++){
-		int index = (*(*it).second.begin());
-
-		if(index - 1 >= 0){
-			nodes[index].insert(index-1);
-		}
-
-		if(index + 1 < n){
-			nodes[index].insert(index+1);
-		}
-
-		it1 = (*it).second.begin();
-		it1++;
-
-		for(it1 = it1; it1 != (*it).second.end(); it1++){
-			nodes[index].insert(*it1);
-		}
-	}
-
-
-	for(i = 0; i < n; i++){
-		if(nodes[i].size() == 0){
-			if(i - 1 >= 0){
-				nodes[i].insert(i-1);
-			}
-
-			if(i + 1 < n){
-				nodes[i].insert(i+1);
-			}
-		}
-	}
-
-	int res = bfs(nodes,n);
+	int res = bfs(n);
 	cout<<res<<endl;
 }

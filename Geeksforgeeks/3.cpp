0e391d3b2d509dc66d4b9cbd242c13d8648bc3dc
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-03-19 15:26:26 +0530

    https://www.geeksforgeeks.org/minimum-edges-reverse-make-path-source-destination/ soln

--- Geeksforgeeks/3.cpp
+++ Geeksforgeeks/3.cpp
@@ -1,127 +1,52 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// void find_min(int dist[5][5], int &srci, int &srcj, bool visited[5][5])
-// {
-// 	int min = INT_MAX;
-// 	int srcc = srci, srjc = srcj;
-// 	if (srcc + 1 < 5)
-// 	{
-// 		if (dist[srcc + 1][srjc] < min && !visited[srcc + 1][srjc])
-// 		{
-// 			min = dist[srcc + 1][srjc];
-// 			srci = srcc + 1;
-// 		}
-// 	}
-
-// 	if (srcc - 1 >= 0)
-// 	{
-// 		if (dist[srcc - 1][srjc] < min && !visited[srcc - 1][srjc])
-// 		{
-// 			min = dist[srcc - 1][srjc];
-// 			srci = srcc - 1;
-// 		}
-// 	}
-
-// 	if (srjc + 1 < 5)
-// 	{
-// 		if (dist[srcc][srjc + 1] < min && !visited[srcc][srjc + 1])
-// 		{
-// 			min = dist[srcc][srjc + 1];
-// 			srcj = srjc + 1;
-// 			srci = srcc;
-// 		}
-// 	}
-
-// 	if (srjc - 1 >= 0)
-// 	{
-// 		if (dist[srcc][srjc - 1] < min && !visited[srcc][srjc - 1])
-// 		{
-// 			min = dist[srcc][srjc - 1];
-// 			srcj = srjc - 1;
-// 			srci = srcc;
-// 		}
-// 	}
-// }
-
-void shortest_path(int grid[5][5], int row, int column)
+int min_no = INT_MAX;
+void find_min_ans(vector<vector<pair<bool,int>>> &graph, vector<bool> &visited, int src, int dest, int no)
 {
-	int dist[5][5];
-	// memset(dist,500,sizeof(int)*row*column);
+	if(visited[src])
+		return;
 
-	int i, j;
-
-	for (i = 0; i < 5; i++)
+	if(src == dest)
 	{
-		for (j = 0; j < 5; j++)
+		if(no < min_no)
 		{
-			dist[i][j] = 1000;
+			min_no = no;
 		}
+		return;
 	}
 
-	dist[0][0] = 0;
+	visited[src] = 1;
 
-	int srci = 0, srcj = 0;
-	bool visited[5][5] = {0};
+	vector<pair<bool,int>>::iterator it;
 
-	priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> q;
-	for (i = 0; i < row; i++)
+	for(it = graph[src].begin(); it != graph[src].end(); it++)
 	{
-		for (j = 0; j < column; j++)
+		if(!visited[it->second])
 		{
-			if (srci + 1 < 5)
-			{
-				if (dist[srci + 1][srcj] > dist[srci][srcj] + grid[srci + 1][srcj])
-				{
-					dist[srci + 1][srcj] = dist[srci][srcj] + grid[srci + 1][srcj];
-				}
-			}
-
-			if (srci - 1 >= 0)
-			{
-				if (dist[srci - 1][srcj] > dist[srci][srcj] + grid[srci - 1][srcj])
-				{
-					dist[srci - 1][srcj] = dist[srci][srcj] + grid[srci - 1][srcj];
-				}
-			}
-
-			if (srcj + 1 < 5)
-			{
-				if (dist[srci][srcj + 1] > dist[srci][srcj] + grid[srci][srcj + 1])
-				{
-					dist[srci][srcj + 1] = dist[srci][srcj] + grid[srci][srcj + 1];
-				}
-			}
-
-			if (srcj - 1 >= 0)
-			{
-				if (dist[srci][srcj - 1] > dist[srci][srcj] + grid[srci][srcj - 1])
-				{
-					dist[srci][srcj - 1] = dist[srci][srcj] + grid[srci][srcj - 1];
-				}
-			}
+			if(it->first == 1)
+				find_min_ans(graph,visited,it->second, dest, no);
+			else
+				find_min_ans(graph,visited,it->second, dest, no+1);
 		}
-	}
-
-	for (i = 0; i < 5; i++)
-	{
-		for (j = 0; j < 5; j++)
-		{
-			cout << dist[i][j] << "\t";
-		}
-		cout << endl;
 	}
 }
 
 int main()
 {
-	int grid[5][5] =
-		{
-			31, 100, 65, 12, 18,
-			10, 13, 47, 157, 6,
-			100, 113, 174, 11, 33,
-			88, 124, 41, 20, 140,
-			99, 32, 111, 41, 20};
+	int edge = 7,nodes = 7;
+	vector<vector<pair<bool,int>>> graph(nodes);
 
-	shortest_path(grid, 5, 5);
+	int i;
+	for(i = 0; i < edge; i++)
+	{
+		int src,dest;
+		cin>>src>>dest;
+		graph[src].push_back({1,dest});
+		graph[dest].push_back({0,src});
+	}
+
+	vector<bool> visited(nodes,false);
+	find_min_ans(graph,visited,0,6,0);
+	cout<<min_no<<endl;
 }

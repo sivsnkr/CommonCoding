53ff3f36a1b50311bae43af28aa5fe99d887c80d
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-03-23 14:30:13 +0530

    update

--- HackerEarth/Circuit/1.cpp
+++ HackerEarth/Circuit/1.cpp
@@ -3,12 +3,33 @@ using namespace std;
 
 int main()
 {
-    // int x,y,p,q;
-    // cin>>x>>y>>p>>q;
+    int t;
+    cin>>t;
 
-    // int b1,b2,b3;
+    for(int i = 0; i < t; i++)
+    {
+        int n;
+        cin>>n;
+        vector<int> numbers(n);
+        int j;
+        for(j = 0; j < n; j++)
+        {
+            cin>>numbers[j];
+        }
 
-    // b1 = 
-    int g = gcd(10,12);
-    cout<<g<<endl;
+        sort(numbers.begin(),numbers.end());
+
+        int min = INT_MAX;
+
+        for(j = 0; j < n-1; j++)
+        {
+            int res = numbers[j]^numbers[j+1];
+            if(res < min)
+            {
+                min = res;
+            }
+        }
+
+        cout<<min<<endl;
+    }
 }

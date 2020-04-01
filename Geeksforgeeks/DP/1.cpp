75a0f8444392dd3d93da6b91f666770ed90f948d
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-04-01 10:01:26 +0530

    first dp problem

--- Geeksforgeeks/DP/1.cpp
+++ Geeksforgeeks/DP/1.cpp
@@ -1,45 +1,52 @@
-# include<bits/stdc++.h> 
-using namespace std; 
+#include<bits/stdc++.h>
+using namespace std;
 
-unsigned getNthUglyNo(unsigned n) 
-{ 
-	unsigned ugly[n];
-	unsigned i2 = 0, i3 = 0, i5 = 0; 
-	unsigned next_multiple_of_2 = 2; 
-	unsigned next_multiple_of_3 = 3; 
-	unsigned next_multiple_of_5 = 5; 
-	unsigned next_ugly_no = 1; 
-
-	ugly[0] = 1; 
-	for (int i=1; i<n; i++) 
-	{ 
-		next_ugly_no = min(next_multiple_of_2, 
-							min(next_multiple_of_3,
-								next_multiple_of_5));
-		ugly[i] = next_ugly_no; 
-		if (next_ugly_no == next_multiple_of_2) 
-		{
-			i2 = i2+1; 
-			next_multiple_of_2 = ugly[i2]*2; 
-		}
-		if (next_ugly_no == next_multiple_of_3) 
-		{
-			i3 = i3+1;
-			next_multiple_of_3 = ugly[i3]*3; 
-		}
-		if (next_ugly_no == next_multiple_of_5) 
-		{
-			i5 = i5+1; 
-			next_multiple_of_5 = ugly[i5]*5;
-		} 
+int getres(int matrix[3][3])
+{
+	int resmatrix[3][3];
+	
+	// populate all the rows first cell with cell value
+	int i;
+	for(i = 0; i < 3; i++)
+	{
+		resmatrix[i][0] = matrix[i][0];
 	}
-	return next_ugly_no; 
-} 
 
-/* Driver program to test above functions */
-int main() 
-{ 
-	int n = 150; 
-	cout << getNthUglyNo(n)<<endl; 
-	return 0; 
-} 
+	// now assign optimall value to all the cell
+
+	int j;
+	for(j = 1; j < 3; j++)
+	{
+		for(i = 0; i < 3; i++)
+		{
+			int value = resmatrix[i][j-1];
+			if(i > 0)
+			{
+				value = max(resmatrix[i-1][j-1],value);
+			}
+
+			if(i+1 < 3)
+				value = max(value,resmatrix[i+1][j-1]);
+			resmatrix[i][j] = matrix[i][j]+value;//max value it can get
+		}
+	}
+
+	// get the max value from last column
+	int returnvalue = 0;
+	for(i = 0; i < 3; i++)
+	{
+		returnvalue = max(returnvalue,resmatrix[i][2]);
+	}
+
+	return returnvalue;
+}
+
+int main()
+{
+	// main driver code
+	int gold[3][3]= {{1, 3, 14},
+                   {2, 1, 4},
+                  {0, 6, 4}};
+	int res = getres(gold);
+	cout<<res<<endl;
+}

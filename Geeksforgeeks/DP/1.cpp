40c39677364a57d904a0ac4f0f8ccbfe0de2736a
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-04-04 11:18:18 +0530

    add golomb-sequence algo

--- Geeksforgeeks/DP/1.cpp
+++ Geeksforgeeks/DP/1.cpp
@@ -1,32 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int getres(int arr[],int n)
+int getres(int arr1[], int arr2[], int n, int i, int size)
 {
-	int dp[n];
-	dp[0] = arr[0];
-	
-	int i;
-	for(i = 1; i < n; i++)
-	{
-		int mxm = 0;
-		int first = i-1;
-		int second = 0;
-		while(first >= second)
-		{
-			mxm = max(mxm,dp[first]+dp[second]);
-			first--;
-			second++;
-		}
-		dp[i] = max(mxm,arr[i]);
-	}
-	return dp[n-1];
+	if (i == size - 1)
+		return n;
+	int res1 = getres(arr1, arr2, n + arr1[i + 1], i + 1, size);
+	int res2 = getres(arr1, arr2, n + arr2[i + 1], i + 1, size);
+	return min(res1, res2);
 }
 
 int main()
 {
-	int arr[] = {1, 5, 8, 9, 10, 17, 17, 20};
-	int size  = sizeof(arr)/sizeof(arr[0]);
-	int res = getres(arr,size);
-	cout<<res<<endl;
+	int firstassembly[] = {4, 5, 3, 2};
+	int secondassembly[] = {2, 10, 1, 4};
+
+	// entry point first assembly
+
+	int res1 = getres(firstassembly, secondassembly, 10 + firstassembly[0], 0, 4);
+	int res2 = getres(secondassembly, firstassembly, 12 + secondassembly[0], 0, 4);
+
+	cout << min(res1, res2) << endl;
 }

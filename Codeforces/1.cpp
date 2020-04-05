adbeaee290735158b5a705c37c68680517930c7c
Shiv Shankar <ced17i026@iiitdm.ac.in>
2020-04-05 19:04:02 +0530

    update

--- Codeforces/1.cpp
+++ Codeforces/1.cpp
@@ -1,28 +1,58 @@
-#include<stdio.h>
-int k[8],a,b,c,r;
-void f(int n){
-    if(n==8){
-        int h=0,A=0,B=0,C=0;
-        for(int i=1;i<8;i++){
-            h+=k[i];
-            A+=k[i]*(i&1);
-            B+=k[i]*((i&2)>>1);
-            C+=k[i]*((i&4)>>2);
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int santa_bag[n];
+        unordered_set<int>gifts;
+
+        int i;
+        for(i = 0; i < n; i++)
+        {
+            cin>>santa_bag[i];
         }
-        if(A<=a&&B<=b&&C<=c&&r<h)r=h;
-        return;
-    }
-    k[n]=0;
-    f(n+1);
-    k[n]=1;
-    f(n+1);
-}
-int main(){
-    int t;scanf("%d",&t);
-    while(t--){
-        scanf("%d%d%d",&a,&b,&c);
-        r=0;
-        f(1);
-        printf("%d\n",r);
+
+        int arr[m];
+        for(i = 0; i < m; i++)
+        {
+            int value;
+            cin>>value;
+            gifts.insert(value);
+            arr[i] = value;
+        }
+
+        unordered_set<int> live;
+
+        long long int sum = 0;
+        int index = 0;
+        for(int j : arr)
+        {
+            if(live.find(j) != live.end())
+            {
+                sum++;
+                live.erase(j);
+            }
+            else
+            {
+                int c = live.size();
+                while(santa_bag[index] != j)
+                {
+                    c++;
+                    live.insert(santa_bag[index]);
+                    index++;
+                }
+                index++;
+                sum+=(2*c+1);
+            }
+        }
+
+        cout<<sum<<endl;
     }
 }
